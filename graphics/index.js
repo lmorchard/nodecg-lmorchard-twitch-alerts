5fdc0e018c7fbe6465bb7857151f59e7be10366f
Les Orchard <me@lmorchard.com>
2020-05-17 13:58:55 -0700

    Rework particle animation system into reusable class

--- graphics/index.js
+++ graphics/index.js
@@ -1,14 +1,24 @@
 const $ = (...args) => document.body.querySelector(...args);
+const appEl = $('#app');
+
+let isRunning = false;
+let particleSystems = [];
+
+const UPDATE_PERIOD = 1000 / 60;
 
 async function init() {
+  reset();
+  update();
+  draw();
+
+  alertFollowing({ from_name: 'ExampleUser' });
+
   nodecg.listenFor('twitch.following', 'twitch-connect', (data) => {
     alertFollowing(data);
   });
-  setTimeout(update, UPDATE_PERIOD);
-  window.requestAnimationFrame(draw);
 }
 
-function alertFollowing(data) {
+async function alertFollowing(data) {
   const { from_name } = data;
 
   nodecg.sendMessageToBundle('say', 'sam-say', {
@@ -19,100 +29,200 @@ function alertFollowing(data) {
 
   $('#alert-following .display-name').innerText = from_name;
 
-  appEl.classList.add('show');
-  resetAnimation();
-  startAnimation();
-
-  setTimeout(() => {
-    setTimeout(() => {
-      stopAnimation();
-    }, 2000);
-    appEl.classList.remove('show');
-    appEl.classList.add('hide');
-  }, 5000);
+  start([new ZoomyShips()]);
+  await show();
+  //await wait(3000);
+  //await hide();
+  //stop();
 }
 
-const color = ({ h, s, l, a = 1.0 }) => ({ h, s, l, a });
-const rnd = (max) => (Math.random() * max);
-const randomColor = () =>
-  color({ h: rnd(1), s: 0.75, l: 0.5, a: 1.0 });
+function show() {
+  appEl.classList.add('show');
+  appEl.classList.remove('hide');
+  return wait(1000);
+}
 
-const UPDATE_PERIOD = 1000 / 60;
+function hide() {
+  appEl.classList.remove('show');
+  appEl.classList.add('hide');
+  return wait(1000);
+}
 
-let isRunning = false;
-let particles = [];
-
-let createParticleInterval = null;
-
-const resetAnimation = () => (particles = []);
-
-const DCOLOR_RANGE = 512;
-const dcolorRnd = () => DCOLOR_RANGE - rnd(DCOLOR_RANGE * 2);
-
-const startAnimation = () => {
-  isRunning = true;
-  if (createParticleInterval) {
-    clearInterval(createParticleInterval);
-  }
-  createParticleInterval = setInterval(() => {
-    const canvas = document.getElementById('canvas');
-    const particle = createParticle({
-      x: -32,
-      y: Math.random() * canvas.height,
-      dx: 500 + Math.random() * 500,
-      dy: 0,
-      ttl: 10,
-      size: 48,
-      color: randomColor(),
-      dcolor: { h: rnd(1.0), s: 0, l: 0, a: 0 },
-    });
-    particles.push(particle);
-  }, 100);
-};
-
-const stopAnimation = () => {
+function reset() {
   isRunning = false;
-  if (createParticleInterval) {
-    clearInterval(createParticleInterval);
-  }
-};
+  particleSystems = [];
+}
 
-let lastUpdate = Date.now();
-function update() {
-  const now = Date.now();
-  const dt = (now - lastUpdate) / 1000;
-  if (isRunning) {
-    for (const particle of particles) {
-      updateParticle(particle, dt);
-    }
+function start(newSystems = []) {
+  isRunning = true;
+  for (const system of particleSystems) {
+    system.start();
   }
-  particles = particles.filter((particle) => particle.alive);
-  lastUpdate = now;
-  window.particles = particles;
+  window.particles = particleSystems = newSystems;
+}
+
+function stop() {
+  isRunning = false;
+  for (const system of particleSystems) {
+    system.stop();
+  }
+}
+
+function update() {
+  const canvas = document.getElementById('canvas');
+  for (const system of particleSystems) {
+    system.update(canvas);
+  }
   setTimeout(update, UPDATE_PERIOD);
 }
 
 function draw() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-
   const cw = 1280; //canvas.parentElement.offsetWidth;
   const ch = 720; //canvas.parentElement.offsetHeight;
   canvas.width = cw;
   canvas.height = ch;
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0)';
   ctx.fillRect(0, 0, cw, ch);
 
-  if (isRunning) {
-    for (const particle of particles) {
-      drawParticle(particle, ctx);
-    }
+  for (const system of particleSystems) {
+    system.draw(canvas);
   }
 
   window.requestAnimationFrame(draw);
 }
 
+class ParticleSystem {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.particles = [];
+    this.running = false;
+    this.lastUpdate = Date.now();
+  }
+
+  start() {
+    this.reset();
+    this.running = true;
+  }
+
+  pause() {
+    this.running = false;
+  }
+
+  update(canvas) {
+    const now = Date.now();
+    const dt = (now - this.lastUpdate) / 1000;
+    this.lastUpdate = now;
+    for (const particle of this.particles) {
+      this.updateParticle(particle, canvas, dt);
+    }
+    this.particles = this.particles.filter((particle) => particle.alive);
+    return dt;
+  }
+
+  draw(canvas) {
+    for (const particle of this.particles) {
+      this.drawParticle(particle, canvas);
+    }
+  }
+
+  updateParticle(particle, canvas, dt) {}
+
+  drawParticle(particle, canvas) {}
+}
+
+class ZoomyShips extends ParticleSystem {
+  constructor({ ...opts } = {}) {
+    super(opts);
+    this.nextParticleDelay = 0;
+  }
+
+  update(canvas) {
+    const dt = super.update(canvas);
+    this.nextParticleDelay -= dt;
+    if (this.nextParticleDelay <= 0) {
+      this.nextParticleDelay = 0.1;
+      this.spawnParticle(canvas);
+    }
+    window.npd = this.nextParticleDelay;
+  }
+
+  spawnParticle(canvas) {
+    this.particles.push({
+      x: -32,
+      y: Math.random() * canvas.height,
+      dx: 500 + Math.random() * 500,
+      dy: 0,
+      ttl: 10,
+      size: 48,
+      color: { h: Math.random(), s: 0.75, l: 0.5, a: 1.0 },
+      dcolor: { h: Math.random(), s: 0, l: 0, a: 0 },
+      size: 32,
+      rotation: 90 * (Math.PI / 180),
+      alive: true,
+    });
+  }
+
+  updateParticle(particle, canvas, dt) {
+    particle.x += particle.dx * dt;
+    particle.y += particle.dy * dt;
+
+    if (particle.x > canvas.width) {
+      particle.alive = false;
+    }
+
+    for (const name of Object.keys(particle.color)) {
+      particle.color[name] =
+        (particle.color[name] + particle.dcolor[name] * dt) % 1.0;
+    }
+
+    particle.ttl -= dt;
+    if (particle.ttl < 0) {
+      particle.alive = false;
+    }
+  }
+
+  drawParticle(particle, canvas) {
+    const ctx = canvas.getContext('2d');
+    const { x, y, color, rotation, size } = particle;
+    const { h, s, l, a } = color;
+
+    ctx.save();
+
+    const [r, g, b] = hslToRgb(h, s, l);
+
+    const rgba = `rgba(${r},${g},${b},1.0)`;
+
+    ctx.lineWidth = 10;
+
+    ctx.translate(x, y);
+    ctx.rotate(rotation);
+    ctx.scale(size / 100, size / 100);
+
+    ctx.strokeStyle = rgba;
+    ctx.beginPath();
+    ctx.moveTo(0, -50);
+    ctx.lineTo(-45, 50);
+    ctx.lineTo(-12.5, 12.5);
+    ctx.lineTo(0, 25);
+    ctx.lineTo(12.5, 12.5);
+    ctx.lineTo(45, 50);
+    ctx.lineTo(0, -50);
+    ctx.moveTo(0, -50);
+    ctx.stroke();
+
+    ctx.restore();
+  }
+}
+
+const wait = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+// https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
 /**
  * Converts an HSL color value to RGB. Conversion formula
  * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
@@ -147,72 +257,6 @@ function hslToRgb(h, s, l) {
   }
 
   return [r * 255, g * 255, b * 255];
-}
-
-function updateParticle(particle, dt) {
-  const canvas = document.getElementById('canvas');
-
-  particle.x += particle.dx * dt;
-  particle.y += particle.dy * dt;
-
-  if (particle.x > canvas.width) {
-    particle.alive = false;
-  }
-
-  for (const name of Object.keys(particle.color)) {
-    particle.color[name] = (particle.color[name] + particle.dcolor[name] * dt) % 1.0;
-  }
-
-  particle.ttl -= dt;
-  if (particle.ttl < 0) {
-    particle.alive = false;
-  }
-}
-
-function createParticle({ x, y, dx, dy, ttl, color, ...rest }) {
-  return {
-    x,
-    y,
-    dx,
-    dy,
-    ttl,
-    color,
-    size: 16,
-    rotation: 90 * (Math.PI / 180),
-    alive: true,
-    ...rest,
-  };
-}
-
-function drawParticle(particle, ctx) {
-  const { x, y, color, rotation, size } = particle;
-  const { h, s, l, a } = color;
-
-  ctx.save();
-
-  const [r, g, b] = hslToRgb(h, s, l);
-
-  const rgba = `rgba(${r},${g},${b},1.0)`;
-
-  ctx.lineWidth = 3;
-
-  ctx.translate(x, y);
-  ctx.rotate(rotation);
-  ctx.scale(size / 100, size / 100);
-
-  ctx.strokeStyle = rgba;
-  ctx.beginPath();
-  ctx.moveTo(0, -50);
-  ctx.lineTo(-45, 50);
-  ctx.lineTo(-12.5, 12.5);
-  ctx.lineTo(0, 25);
-  ctx.lineTo(12.5, 12.5);
-  ctx.lineTo(45, 50);
-  ctx.lineTo(0, -50);
-  ctx.moveTo(0, -50);
-  ctx.stroke();
-
-  ctx.restore();
 }
 
 init();
